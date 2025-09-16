1fe4a36b882998bdbab531c966b32dc0ed9f0841
AndresMHG <andr3sm1guel@gmail.com>
2025-09-16 20:25:07 -0300

    fix header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    fonts: [{
+        provider: fontProviders.google(),
+          name: "Inter",
+          cssVariable: "--font-inter"
+      }]
+  },
   integrations: [vue()]
 });
